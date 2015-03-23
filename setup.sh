e93c54799848e7bc4571ad616fa82d0566a83bee
JustDolanIt <4epaeb@gmail.com>
2015-03-23 10:22:33 +0300

    Well, screen is not that good

--- setup.sh
+++ setup.sh
@@ -4,6 +4,3 @@ cp ./bash/.bashrc $HOME/.bashrc
 # Vim config
 cp ./vim/.vimrc $HOME/.vimrc
 vim ./vim/markdown-1.2.2.vba +":source %"
-# Screen config
-cp ./screen/.bash_profile $HOME/.bash_profile
-cp ./screen/.screenrc $HOME/.screenrc
